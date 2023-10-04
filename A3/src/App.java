2409ec913fbdc3f39ea1d00baebb47aa89d38c54
DhomynyFariasDeSouza <dhomyryan@outlook.com>
2023-10-04 11:03:50 -0300

    GAS!!

--- A3/src/App.java
+++ A3/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        long i = 0;
+        while(true){
+                
+                System.out.println(i);
+                i++;
+                Thread.sleep(1);
+        }
     }
 }
